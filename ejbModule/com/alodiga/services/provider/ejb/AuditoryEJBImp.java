488174aae1bb475ab7d6a9b62a440ed6677772fd
yamealm <yamealm@gmail.com>
2019-08-28 22:31:24 -0400

    auditoria

--- ejbModule/com/alodiga/services/provider/ejb/AuditoryEJBImp.java
+++ ejbModule/com/alodiga/services/provider/ejb/AuditoryEJBImp.java
@@ -1,6 +1,8 @@
 package com.alodiga.services.provider.ejb;
 
 import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
@@ -12,11 +14,15 @@ import com.alodiga.services.provider.commons.exceptions.GeneralException;
 import com.alodiga.services.provider.commons.exceptions.NullParameterException;
 import com.alodiga.services.provider.commons.exceptions.RegisterNotFoundException;
 import com.alodiga.services.provider.commons.genericEJB.AbstractSPEJB;
+import com.alodiga.services.provider.commons.genericEJB.EJBRequest;
 import com.alodiga.services.provider.commons.genericEJB.SPContextInterceptor;
 import com.alodiga.services.provider.commons.genericEJB.SPLoggerInterceptor;
+import com.alodiga.services.provider.commons.models.Audit;
 import com.alodiga.services.provider.commons.models.AuditAction;
 import com.alodiga.services.provider.commons.utils.EjbConstants;
+import com.alodiga.services.provider.commons.utils.EjbUtils;
 import com.alodiga.services.provider.commons.utils.GeneralUtils;
+import com.alodiga.services.provider.commons.utils.QueryConstants;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -34,70 +40,70 @@ public class AuditoryEJBImp extends AbstractSPEJB implements AuditoryEJB, Audito
 //        return null;
 //    }
 //
-//    public List<Audit> getLastAudits(EJBRequest request) throws GeneralException, RegisterNotFoundException, NullParameterException, EmptyListException {
-//        List<Audit> audits = null;
-//        String nameQuery = null;
-//        nameQuery = QueryConstants.LOAD_LAST_AUDITS;
-//        audits = (List<Audit>) getNamedQueryResult(Audit.class, nameQuery, request, getMethodName(), logger, "audits");
-//        return audits;
-//
-//    }
-//
-//    public List<Audit> getAudits(EJBRequest request) throws GeneralException, RegisterNotFoundException, NullParameterException, EmptyListException {
-//        return (List<Audit>) listEntities(Audit.class, request, logger, getMethodName());
-//    }
-//
-//    public Audit loadAudit(EJBRequest request) throws GeneralException, RegisterNotFoundException, NullParameterException {
-//        return (Audit) loadEntity(Audit.class, request, logger, getMethodName());
-//    }
-//
-//    public Audit saveAudit(EJBRequest request) throws GeneralException, NullParameterException {
-//        return (Audit) saveEntity(request, logger, getMethodName());
-//    }
-//
-//    public List<Audit> searchAudit(EJBRequest request) throws GeneralException, NullParameterException, EmptyListException {
-//        List<Audit> audits = new ArrayList<Audit>();
-//        Map<String, Object> params = request.getParams();
-//        String sql = "SELECT a FROM Audit a WHERE a.creationDate BETWEEN :date1 AND :date2";
-//
-//        if (!params.containsKey(QueryConstants.PARAM_BEGINNING_DATE) || !params.containsKey(QueryConstants.PARAM_ENDING_DATE)) {
-//            throw new NullParameterException( sysError.format(EjbConstants.ERR_NULL_PARAMETER, this.getClass(), getMethodName(), "beginningDate & endingDate"), null);
-//        }
-//        if (params.containsKey(QueryConstants.PARAM_RESPONSIBLE_ID)) {
-//            sql += " AND a.responsibleId LIKE '%" + params.get(QueryConstants.PARAM_RESPONSIBLE_ID) + "%'";
-//        }
-//        if (params.containsKey(QueryConstants.PARAM_RESPONSIBLE_TYPE)) {
-//            sql += " AND a.responsibleType LIKE '%" + params.get(QueryConstants.PARAM_RESPONSIBLE_TYPE) + "%'";
-//        }
-//        if (params.containsKey(QueryConstants.PARAM_TABLE_NAME)) {
-//            sql += " AND a.tableName LIKE '%" + params.get(QueryConstants.PARAM_TABLE_NAME) + "%'";
-//        }
-//        if (params.containsKey(QueryConstants.PARAM_REMOTE_IP)) {
-//            sql += " AND a.remoteIp LIKE '%" + params.get(QueryConstants.PARAM_REMOTE_IP) + "%'";
-//        }
-//        if (params.containsKey(QueryConstants.PARAM_EVENT_ID)) {
-//            sql += " AND a.event.id =" + params.get(QueryConstants.PARAM_EVENT_ID);
-//        }
-//        Query query = null;
-//        try {
-//            query = createQuery(sql);
-//            query.setParameter("date1", EjbUtils.getBeginningDate((Date) params.get(QueryConstants.PARAM_BEGINNING_DATE)));
-//            query.setParameter("date2", EjbUtils.getEndingDate((Date) params.get(QueryConstants.PARAM_ENDING_DATE)));
-//
-//            if (request.getLimit() != null && request.getLimit() > 0) {
-//                query.setMaxResults(request.getLimit());
-//            }
-//            audits = query.setHint("toplink.refresh", "true").getResultList();
-//
-//        } catch (Exception e) {
-//            throw new GeneralException(logger, sysError.format(EjbConstants.ERR_GENERAL_EXCEPTION, this.getClass(), getMethodName(), e.getMessage()), null);
-//        }
-//        if (audits.isEmpty()) {
-//            throw new EmptyListException(logger, sysError.format(EjbConstants.ERR_EMPTY_LIST_EXCEPTION, this.getClass(), getMethodName()), null);
-//        }
-//        return audits;
-//
-//    }
+    public List<Audit> getLastAudits(EJBRequest request) throws GeneralException, RegisterNotFoundException, NullParameterException, EmptyListException {
+        List<Audit> audits = null;
+        String nameQuery = null;
+        nameQuery = QueryConstants.LOAD_LAST_AUDITS;
+        audits = (List<Audit>) getNamedQueryResult(Audit.class, nameQuery, request, getMethodName(), logger, "audits");
+        return audits;
+
+    }
+
+    public List<Audit> getAudits(EJBRequest request) throws GeneralException, RegisterNotFoundException, NullParameterException, EmptyListException {
+        return (List<Audit>) listEntities(Audit.class, request, logger, getMethodName());
+    }
+
+    public Audit loadAudit(EJBRequest request) throws GeneralException, RegisterNotFoundException, NullParameterException {
+        return (Audit) loadEntity(Audit.class, request, logger, getMethodName());
+    }
+
+    public Audit saveAudit(EJBRequest request) throws GeneralException, NullParameterException {
+        return (Audit) saveEntity(request, logger, getMethodName());
+    }
+
+    public List<Audit> searchAudit(EJBRequest request) throws GeneralException, NullParameterException, EmptyListException {
+        List<Audit> audits = new ArrayList<Audit>();
+        Map<String, Object> params = request.getParams();
+        String sql = "SELECT a FROM Audit a WHERE a.creationDate BETWEEN :date1 AND :date2";
+
+        if (!params.containsKey(QueryConstants.PARAM_BEGINNING_DATE) || !params.containsKey(QueryConstants.PARAM_ENDING_DATE)) {
+            throw new NullParameterException( sysError.format(EjbConstants.ERR_NULL_PARAMETER, this.getClass(), getMethodName(), "beginningDate & endingDate"), null);
+        }
+        if (params.containsKey(QueryConstants.PARAM_RESPONSIBLE_ID)) {
+            sql += " AND a.responsibleId LIKE '%" + params.get(QueryConstants.PARAM_RESPONSIBLE_ID) + "%'";
+        }
+        if (params.containsKey(QueryConstants.PARAM_RESPONSIBLE_TYPE)) {
+            sql += " AND a.responsibleType LIKE '%" + params.get(QueryConstants.PARAM_RESPONSIBLE_TYPE) + "%'";
+        }
+        if (params.containsKey(QueryConstants.PARAM_TABLE_NAME)) {
+            sql += " AND a.tableName LIKE '%" + params.get(QueryConstants.PARAM_TABLE_NAME) + "%'";
+        }
+        if (params.containsKey(QueryConstants.PARAM_REMOTE_IP)) {
+            sql += " AND a.remoteIp LIKE '%" + params.get(QueryConstants.PARAM_REMOTE_IP) + "%'";
+        }
+        if (params.containsKey(QueryConstants.PARAM_EVENT_ID)) {
+            sql += " AND a.event.id =" + params.get(QueryConstants.PARAM_EVENT_ID);
+        }
+        Query query = null;
+        try {
+            query = createQuery(sql);
+            query.setParameter("date1", EjbUtils.getBeginningDate((Date) params.get(QueryConstants.PARAM_BEGINNING_DATE)));
+            query.setParameter("date2", EjbUtils.getEndingDate((Date) params.get(QueryConstants.PARAM_ENDING_DATE)));
+
+            if (request.getLimit() != null && request.getLimit() > 0) {
+                query.setMaxResults(request.getLimit());
+            }
+            audits = query.setHint("toplink.refresh", "true").getResultList();
+
+        } catch (Exception e) {
+            throw new GeneralException(logger, sysError.format(EjbConstants.ERR_GENERAL_EXCEPTION, this.getClass(), getMethodName(), e.getMessage()), null);
+        }
+        if (audits.isEmpty()) {
+            throw new EmptyListException(logger, sysError.format(EjbConstants.ERR_EMPTY_LIST_EXCEPTION, this.getClass(), getMethodName()), null);
+        }
+        return audits;
+
+    }
     public AuditAction deleteAuditAction(Long actionId) throws GeneralException, NullParameterException {
         return null;
     }
